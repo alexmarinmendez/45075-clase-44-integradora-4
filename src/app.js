7ac34441fa6c6eb651aba526a06e92ae19709aa0
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-06-21 19:16:15 -0500

    feat(2): add swagger documentation

--- src/app.js
+++ src/app.js
@@ -2,11 +2,26 @@ import express from 'express'
 import router from './routers/pizza.router.js'
 import mongoose from 'mongoose'
 import { loggerHttp } from './logger.js'
+import swaggerJSDoc from 'swagger-jsdoc'
+import swaggerUiExpress from 'swagger-ui-express'
+import __dirname from './utils.js'
+
+const specs = swaggerJSDoc({
+    definition: {
+        openapi: '3.0.1',
+        info: {
+            title: 'Documentación de la API de Pizzas',
+            description: 'Details'
+        }
+    },
+    apis: [`${__dirname}/../docs/**/*.yaml`]
+})
 
 const app = express()
 app.use(express.json())
 app.use(loggerHttp)
 
+app.use('/doc', swaggerUiExpress.serve, swaggerUiExpress.setup(specs))
 app.use('/api/pizza', router)
 app.use('/', (req, res) => res.json({ status: 'ok' }))
 
